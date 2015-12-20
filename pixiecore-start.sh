cb163f52752a23277603e49fec6d1f1156e4c21b
Nemo HJ Lee <hunjae.lee@gmail.com>
2015-12-20 20:56:21 +0900

    add autologin

--- pixiecore-start.sh
+++ pixiecore-start.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker run -d --name pixiecore --restart=always -v /data/docker-volumes/pxe-image:/image --net=host danderson/pixiecore -kernel /image/coreos_production_pxe.vmlinuz -initrd /image/coreos_production_pxe_image.cpio.gz
+docker run -d --name pixiecore --restart=always -v /data/docker-volumes/pxe-image:/image --net=host danderson/pixiecore -kernel /image/coreos_production_pxe.vmlinuz -initrd /image/coreos_production_pxe_image.cpio.gz --cmdline coreos.autologin 
